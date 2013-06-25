e14c9223ac6b7acea72803ebc7cee692a2d7cf5a
Leliel Trethowen <leliel@ecs.vuw.ac.nz>
2013-06-25 14:27:06 +1200

    added test cases for parsing

---
+++ 489Parser/src/tests/TestInvalidParsing.java
@@ -0,0 +1,59 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import Parser.Parser;
+import dataTypes.Invalid;
+import dataTypes.Server;
+import dataTypes.User;
+
+public class TestInvalidParsing {
+
+	private Parser p;
+	private SimpleDateFormat d, t;
+	
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+	
+	@Before
+	public void setUp() {
+		p = new Parser();
+		d = new SimpleDateFormat("MMM dd", Locale.ENGLISH);
+		t = new SimpleDateFormat("HH:mm:ss");
+	}
+
+	@Test
+	public void testInvalidParseGood() {
+		String input = "Apr 19 04:36:49 app-1 sshd[6990]: Invalid user tomcat from 203.81.226.86";
+		try {
+			Invalid output = 
+					new Invalid(
+							new Date(d.parse("Apr 19").getTime()),
+							new Time(t.parse("04:36:49").getTime()),
+							new Server("app-1", ""),
+							6990,
+							new User("user1", false),
+							"203.81.226.86",
+							"Apr 19 04:36:49 app-1 sshd[6990]: Invalid user tomcat from 203.81.226.86");
+					
+
+			assertEquals("parsing Failed, object do not match", output,
+					p.parseLine(input));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			fail("Parsing threw an exception.");
+		}
+	}
+}
---
+++ 489Parser/src/tests/TestOtherParsing.java
@@ -0,0 +1,67 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import Parser.Parser;
+import dataTypes.Other;
+import dataTypes.Server;
+
+public class TestOtherParsing {
+
+
+	private Parser p;
+	private SimpleDateFormat d, t;
+	
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+	
+	@Before
+	public void setUp() {
+		p = new Parser();
+		d = new SimpleDateFormat("MMM dd", Locale.ENGLISH);
+		t = new SimpleDateFormat("HH:mm:ss");
+	}
+
+	@Test
+	public void testOtherParseGood() {
+		String[] input = {"Mar 16 08:25:22 app-1 sshd[4884]: Server listening on :: port 22.",
+						"Mar 16 08:25:22 app-1 sshd[4884]: error: Bind to port 22 on 0.0.0.0 failed: Address already in use."};
+		try {
+			Other[] output = { 
+					new Other(
+							new Date(d.parse("Mar 16").getTime()),
+							new Time(t.parse("08:25:22").getTime()),
+							new Server("app-1", ""),
+							4884,
+							"Server listening on :: port 22.",
+							"Mar 16 08:25:22 app-1 sshd[4884]: Server listening on :: port 22."),
+					new Other(new Date(d.parse("Mar 16").getTime()),
+							new Time(t.parse("08:25:22").getTime()),
+							new Server("app-1", ""),
+							4884,
+							"error: Bind to port 22 on 0.0.0.0 failed: Address already in use.",
+							"Mar 16 08:25:22 app-1 sshd[4884]: error: Bind to port 22 on 0.0.0.0 failed: Address already in use.")};
+					
+
+			assertEquals("parsing Failed, object do not match", output[0],
+					p.parseLine(input[0]));
+			assertEquals("parsing Failed, object do not match", output[1],
+					p.parseLine(input[1]));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			fail("Parsing threw an exception.");
+		}
+	}
+}
